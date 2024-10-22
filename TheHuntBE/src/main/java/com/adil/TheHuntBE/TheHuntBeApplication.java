0064b2aad52ed701b8424d4a0cc70ee79332ea31
adilxem <adil.shakeel104@gmail.com>
2024-10-22 15:53:23 +0530

    initial backend files

---
+++ TheHuntBE/src/main/java/com/adil/TheHuntBE/TheHuntBeApplication.java
@@ -0,0 +1,13 @@
+package com.adil.TheHuntBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheHuntBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheHuntBeApplication.class, args);
+	}
+
+}
---
+++ TheHuntBE/src/test/java/com/adil/TheHuntBE/TheHuntBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adil.TheHuntBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheHuntBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
